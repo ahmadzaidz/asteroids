98ace6104a8ed8dea473f974eaaf465213e8b3ad
ahmadzaidz <23734664+ahmadzaidz@users.noreply.github.com>
2026-01-26 22:57:22 +0800

    set frame rate

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
     while True: 
         log_state()
         for event in pygame.event.get():
@@ -14,6 +16,9 @@ def main():
                 return
         screen.fill("black")
         pygame.display.flip()
+        clock.tick(60)
+        dt = clock.tick(60)/1000
+        
 
 
 if __name__ == "__main__":
